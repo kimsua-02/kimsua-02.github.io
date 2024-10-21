d918148a7376ebbb6ff37b31a214a7d9a73fcde2
kimsua-02 <k021113a@naver.com>
2024-10-21 13:16:18 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>안녕하세요 ^_^</h1>
+      <h2>포트폴리오 입니다람쥐 </h2>
+    </>
+  );
+}
+
+export default App;
